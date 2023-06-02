84cbb085ccea6bd49dc96c4f041760ca68dbdd0e
piop2 <seungyounyou0220@gmail.com>
2023-06-03 05:47:55 +0900

    update clip and clips: scale up image

--- src/util/image.py
+++ src/util/image.py
@@ -9,13 +9,13 @@ def load_images(path: str, rects: list[tuple[int, int, int, int]], scale: int = 
     return [pygame.transform.scale_by(surf, scale) for surf in clips(pygame.image.load(path), rects)]
 
 
-def clip(surf: pygame.Surface, rect: tuple[int, int, int, int]) -> pygame.Surface:
+def clip(surf: pygame.Surface, rect: tuple[int, int, int, int], scale: int = 1) -> pygame.Surface:
     handle_surf = surf.copy()
     clip_rect = pygame.Rect(*rect)
     handle_surf.set_clip(clip_rect)
     image = surf.subsurface(handle_surf.get_clip())
-    return image.copy()
+    return pygame.transform.scale_by(image.copy(), scale)
 
 
-def clips(surf: pygame.Surface, rects: list[tuple[int, int, int, int]]) -> list[pygame.Surface]:
-    return [clip(surf, rect) for rect in rects]
+def clips(surf: pygame.Surface, rects: list[tuple[int, int, int, int]], scale: int = 1) -> list[pygame.Surface]:
+    return [clip(surf, rect, scale) for rect in rects]
