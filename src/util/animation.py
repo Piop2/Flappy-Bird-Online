b075c7657bb1c610545498f6fa425f19942560d6
piop2 <seungyounyou0220@gmail.com>
2023-06-03 05:44:07 +0900

    add animation module

---
+++ src/util/animation.py
@@ -0,0 +1,56 @@
+import json
+
+import pygame
+
+from src.util.image import clips
+
+
+class Animation:
+    def __init__(self, images: list[pygame.Surface], durations: list[int]):
+        self.images = images
+        self.durations = durations
+
+        self.frame = 0
+        self.timer = 0
+        self.playing = True
+        return
+
+    @classmethod
+    def load(cls, path: str, scale: int = 1):
+        with open(path, "r") as f:
+            ani_data = json.load(f)
+
+        frame_data = []
+        durations = []
+        for frame in ani_data["frames"]:
+            frame_data.append(tuple(frame["frame"].values()))
+            durations.append(frame["duration"])
+
+        sprites = pygame.image.load(ani_data["meta"]["image"])
+        frame_images = clips(sprites, frame_data)
+        return cls(images=frame_images, durations=durations)
+
+    def pause(self):
+        self.playing = False
+        return
+
+    def play(self):
+        self.playing = True
+        return
+
+    @property
+    def image(self) -> pygame.Surface:
+        return self.images[self.frame]
+
+    def update(self, dt: int):
+        if not self.playing:
+            return
+
+        self.timer += dt
+        if self.timer >= self.durations[self.frame]:
+            self.timer = 0
+
+            self.frame += 1
+            if self.frame == len(self.images):
+                self.frame = 0
+        return
