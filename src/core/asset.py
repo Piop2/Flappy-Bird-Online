1c7f8ec8f1f70a0205b57e410d640647d7609507
piop2 <seungyounyou0220@gmail.com>
2023-06-05 11:24:06 +0900

    add rotate function

--- src/core/asset.py
+++ src/core/asset.py
@@ -1,8 +1,6 @@
 from src.util.animation import Animation
 from src.util.image import load_image, load_images
 
-IMAGE_SCALE = 3
-
 
 class Asset:
     def __init__(self):
@@ -22,21 +20,21 @@ class Font:
 
 class Icon:
     def __init__(self):
-        self.flappy29 = load_image("asset/icon/flappy29.png", 1)
+        self.flappy29 = load_image("asset/icon/flappy29.png")
 
 
 class Image:
     def __init__(self):
-        self.background = load_image("asset/image/background.png", IMAGE_SCALE)
-        self.floor = load_image("asset/image/floor.png", IMAGE_SCALE)
+        self.background = load_image("asset/image/background.png")
+        self.floor = load_image("asset/image/floor.png")
 
-        titles = load_images("asset/image/titles.png", [(0, 0, 96, 22), (0, 26, 94, 19), (0, 48, 87, 22)], IMAGE_SCALE)
+        titles = load_images("asset/image/titles.png", [(0, 0, 96, 22), (0, 26, 94, 19), (0, 48, 87, 22)])
         self.title_flappy_bird = titles[0]
         self.title_game_over = titles[1]
         self.title_get_ready = titles[2]
 
         # animation
-        self.bird_fly_ani = Animation.load("asset/animation/bird.json", IMAGE_SCALE)
+        self.bird_fly_ani = Animation.load("asset/animation/bird.json")
 
 
 class Sound:
--- src/util/image.py
+++ src/util/image.py
@@ -1,21 +1,50 @@
 import pygame
 
 
-def load_image(path: str, scale: int = 1) -> pygame.Surface:
-    return pygame.transform.scale_by(pygame.image.load(path), scale)
+def load_image(path: str) -> pygame.Surface:
+    return pygame.transform.scale_by(pygame.image.load(path))
 
 
-def load_images(path: str, rects: list[tuple[int, int, int, int]], scale: int = 1) -> list[pygame.Surface]:
-    return [pygame.transform.scale_by(surf, scale) for surf in clips(pygame.image.load(path), rects)]
+def load_images(path: str, rects: list[tuple[int, int, int, int]]) -> list[pygame.Surface]:
+    return [pygame.transform.scale_by(surf) for surf in clips(pygame.image.load(path), rects)]
 
 
-def clip(surf: pygame.Surface, rect: tuple[int, int, int, int], scale: int = 1) -> pygame.Surface:
+def clip(surf: pygame.Surface, rect: tuple[int, int, int, int]) -> pygame.Surface:
     handle_surf = surf.copy()
     clip_rect = pygame.Rect(*rect)
     handle_surf.set_clip(clip_rect)
     image = surf.subsurface(handle_surf.get_clip())
-    return pygame.transform.scale_by(image.copy(), scale)
+    return image.copy()
 
 
-def clips(surf: pygame.Surface, rects: list[tuple[int, int, int, int]], scale: int = 1) -> list[pygame.Surface]:
-    return [clip(surf, rect, scale) for rect in rects]
+def clips(surf: pygame.Surface, rects: list[tuple[int, int, int, int]]) -> list[pygame.Surface]:
+    return [clip(surf, rect) for rect in rects]
+
+
+def rotate(image, pos, originPos, angle):
+    """
+    :param image: 이미지
+    :param pos: 위치
+    :param originPos: 회전 위치
+    :param angle: 각도
+    :return : 회전된 이미지
+    """
+
+    # calculate the axis aligned bounding box of the rotated image
+    w, h = image.get_size()
+    box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
+    box_rotate = [p.rotate(angle) for p in box]
+    min_box = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1])
+    max_box = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1])
+
+    # calculate the translation of the pivot
+    pivot = pygame.math.Vector2(originPos[0], -originPos[1])
+    pivot_rotate = pivot.rotate(angle)
+    pivot_move = pivot_rotate - pivot
+
+    # calculate the upper left origin of the rotated image
+    origin = (pos[0] - originPos[0] + min_box[0] - pivot_move[0], pos[1] - originPos[1] - max_box[1] + pivot_move[1])
+
+    # get a rotated image
+    rotated_image = pygame.transform.rotate(image, angle)
+    return rotated_image, origin
