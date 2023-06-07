50635f4ddeec108e658d15aacbf319739b31759b
piop2 <seungyounyou0220@gmail.com>
2023-06-07 19:10:23 +0900

    update: Entity can check collide & rotate

--- src/object/_entity.py
+++ src/object/_entity.py
@@ -3,13 +3,23 @@ import pygame
 from src.object._object import Object
 from src.util.animation import Animation
 from src.object._hitbox import HitBox
+from src.util.image import rotate
 
 
 class Entity(Object):
     def __init__(self, pos: list[int, int], image: pygame.Surface = None, animation: Animation = None,
-                 hitbox: HitBox = None):
+                 hitbox: HitBox = None, rotate_pos: tuple[int, int] = (0, 0)):
         super().__init__(pos, image, animation)
         self.hitbox = hitbox
+        self.angle = 0
+        self.rotate_pos = rotate_pos
+        return
+
+    def is_collide(self, pos: list[int, int], hitbox_list: list[HitBox]):
+        return self.hitbox.is_collide(pos, hitbox_list)
+
+    def render(self, display: pygame.Surface):
+        display.blit(rotate(self.get_image(), self.pos, self.rotate_pos, self.angle))
         return
 
 
